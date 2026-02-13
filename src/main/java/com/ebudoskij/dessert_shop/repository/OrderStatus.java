8b3bd62a9eaab559c4e0b960d1429cc5e4fba6e1
Yehor Budovskyi <yehor_b@Yehors-MacBook-Air.local>
2026-02-13 10:14:07 +0200

    added customizable to product

--- src/main/java/com/ebudoskij/dessert_shop/repository/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.ebudoskij.dessert_shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderStatus extends JpaRepository<OrderStatus, Long> {
-}
